08fda19207f47fd5334d9fe6ceba8a80ae5b7252
AMD <michojekunle1@gmail.com>
2023-06-03 21:05:55 +0100

    made changes

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,23 @@
-import { createApi, fetchBaseQuery } from '@redux/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: `https://article-extractor-and-summarizer.p.rapidapi.com/`,
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+        })
+    })
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;https://github.com/adrianhajdin/project_ai_summarizer/tree/main/src/services
