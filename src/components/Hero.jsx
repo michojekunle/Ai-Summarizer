471dd0b8369640ce1ce1d7686cbfaed61f8c7bab
AMD <90222287+michojekunle@users.noreply.github.com>
2024-04-18 10:14:11 +0100

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
             </button>
         </nav>
         <h1 className='head_text'>Summarize Articles with <br/> <span className='orange_gradient'>OpenAI GPT-4</span></h1>
-        <h2 className='desc'>Simplify your reading with Summarize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.</h2>
+        <h2 className='desc'>Simplify your reading with Sum-z, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.</h2>
     </header>
   )
 }
